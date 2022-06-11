e285bcd93d9730e937cca49b8e7b842b5dff0063
Artha-Shonen <ydecorreia@gmail.com>
2022-06-10 21:29:34 -0300

    10-06 aula

---
+++ contatos.php
@@ -0,0 +1,22 @@
+<?php
+
+$nome= $_GET['nome'];
+$email= $_GET["email"];
+$tipo= $_GET["tipo-contato"];
+$n1= $_GET["1"];
+$n2= $_GET["2"];
+
+
+echo $nome;
+echo "<br/>";
+echo $email;
+echo "<br/>";
+echo $tipo;
+echo $n1;
+echo "<br/>";
+echo $n2;
+
+/*echo $_POST["1"]+ $_POST["2"]; */
+/*echo 'meu nome é coutinho'; */
+
+/*var_dump($_POST);*/
