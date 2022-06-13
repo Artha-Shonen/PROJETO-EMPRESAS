bf5adb6500e06bb3b4b82b0531f2c24b5efe19cb
Artha-Shonen <ydecorreia@gmail.com>
2022-06-13 17:17:47 -0300

    Atualizado 13-06

--- contatos.php
+++ contatos.php
@@ -3,26 +3,42 @@
 
 
 $nome= $_POST['nome'];
+$sobrenome= $_POST['sobrenome'];
+$telefone= $_POST['telefone'];
 $email= $_POST["email"];
-$tipo= $_POST["tipo-contato"];
-$n1= $_POST["1"];
-$n2= $_POST["2"];
+$arrobas= $_POST["arrobas"];
+$datanascimento= $_POST['data'];
+$senha= $_POST['senha'];
+$confirmasenha= $_POST['confirmasenha'];
 
-echo $nome;
+
+echo "Nome: ".$nome;
+echo $sobrenome;
 echo "<br/>";
-echo $email;
+echo "Nº de telefone: ".$telefone ;
+echo '<br/>';
+echo "Email:".$email;
+echo $arrobas;
 echo "<br/>";
-echo $tipo;
-echo $n1;
+echo "Nascido no dia: ".$datanascimento;
 echo "<br/>";
-echo $n2;
+echo $senha; 
+echo '<br/>';
+echo  $confirmasenha; 
 
 
 
 
+if($_POST) {
+    $senha= $_POST['senha'];
+    $confirmasenha= $_POST['confirmasenha'];
+        if ($confirmasenha != $senha) {
+            $mensagem = "<span class='aviso'><b>Erro!!</b>: Senha não confere!</span>";
+        } else {
+            $mensagem = "<span class='erro'><b>Sucesso!</b>: As senhas conferem!</span>";
+        }
+        echo "<p id='mensagem'>".$mensagem."</p>";
+ }
+?>
 
 
-/*echo $_POST["1"]+ $_POST["2"]; */
-/*echo 'meu nome é coutinho'; */
-
-/*var_dump($_POST);*/
